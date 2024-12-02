903d993892e5b51d451ad44d1383533449fb0e61
Jake Patten <jtp62161@uga.edu>
2024-12-02 13:19:19 -0500

    Created visuals for first screen, allows user to input location and date

--- src/main/java/cs1302/api/ApiApp.java
+++ src/main/java/cs1302/api/ApiApp.java
@@ -2,55 +2,91 @@ package cs1302.api;
 
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.VBox;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
-/**
- * REPLACE WITH NON-SHOUTING DESCRIPTION OF YOUR APP.
- */
 public class ApiApp extends Application {
-    Stage stage;
     Scene scene;
-    VBox root;
 
-    /**
-     * Constructs an {@code ApiApp} object. This default (i.e., no argument)
-     * constructor is executed in Step 2 of the JavaFX Application Life-Cycle.
-     */
+    /* FIRST SCREEN */
+    StackPane firstScreen;
+    VBox firstVBox;
+    Label titleLabel;
+    HBox options;
+    VBox optionLabels, optionFields;
+    Label option1Label, option2Label;
+    TextField option1Field, option2Field;
+    Button searchButton;
+
+    /* SECOND SCREEN */
+
+
     public ApiApp() {
-        root = new VBox();
-    } // ApiApp
+        /* FIRST SCREEN */
+        this.firstScreen = new StackPane();
+        this.firstVBox = new VBox();
+        this.titleLabel = new Label("Travel Companion");
+        this.titleLabel.setFont(Font.font("Comic Sans", FontWeight.BOLD, 34));
+        this.options = new HBox(10);
+        this.optionLabels = new VBox(8);
+        this.option1Label = new Label("City (Ex. Athens, GA):");
+        this.option1Label.setFont(Font.font("Comic Sans", 18));
+        this.option2Label = new Label("Start Date (Ex. MM/DD):");
+        this.option2Label.setFont(Font.font("Comic Sans", 18));
+        this.optionFields = new VBox(8);
+        this.option1Field = new TextField();
+        this.option2Field = new TextField();
+        this.searchButton = new Button("Search");
+        this.searchButton.setFont(Font.font(18));
 
-    /** {@inheritDoc} */
+        /* SECOND SCREEN */
+
+
+    } // App
+
     @Override
     public void start(Stage stage) {
+        /* ADD NODES TO PARENTS */
+        // first screen
+        this.firstScreen.getChildren().add(firstVBox);
+        this.firstVBox.getChildren().addAll(titleLabel, options, searchButton);
+        this.options.getChildren().addAll(optionLabels, optionFields);
+        this.optionLabels.getChildren().addAll(option1Label, option2Label);
+        this.optionFields.getChildren().addAll(option1Field, option2Field);
+        // second screen
 
-        this.stage = stage;
+        /* FORMAT */
+        // first screen
+        this.firstScreen.setMinSize(500, 400);
+        this.firstVBox.setAlignment(Pos.BASELINE_CENTER);
+        this.firstVBox.setPadding(new Insets(40));
+        this.options.setPadding(new Insets(50, 0, 30, 0));
+        this.optionLabels.setMinWidth(firstScreen.getMinWidth() / 2);
+        this.optionFields.setMinWidth(firstScreen.getMinWidth() / 2);
+        this.optionLabels.setAlignment(Pos.CENTER_RIGHT);
+        this.optionFields.setAlignment(Pos.CENTER_LEFT);
 
-        // demonstrate how to load local asset using "file:resources/"
-        Image bannerImage = new Image("file:resources/readme-banner.png");
-        ImageView banner = new ImageView(bannerImage);
-        banner.setPreserveRatio(true);
-        banner.setFitWidth(640);
+        // temp coloring
 
-        // some labels to display information
-        Label notice = new Label("Modify the starter code to suit your needs.");
 
-        // setup scene
-        root.getChildren().addAll(banner, notice);
-        scene = new Scene(root);
-
-        // setup stage
-        stage.setTitle("ApiApp!");
+        /* SETUP SCENE & STAGE */
+        this.scene = new Scene(firstScreen);
         stage.setScene(scene);
+        stage.setTitle("ApiApp!");
         stage.setOnCloseRequest(event -> Platform.exit());
+        stage.setResizable(false);
         stage.sizeToScene();
         stage.show();
-
     } // start
 
 } // ApiApp
